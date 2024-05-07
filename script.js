42988c0f734a71bcef8e7c643eb2e65232d96ba4
Ni0j <jinj952@newschool.edu>
2024-05-06 20:23:47 -0400

    revise gutter effect

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const leftPane = document.querySelector(".cd-container");
+    const rightPane = document.querySelector(".fa-container");
+    const gutter = document.querySelector(".gutter");
+    leftPane.style.width = window.innerWidth / 2 - 30 + "px";
+    
+    function resizer(e) {
+      
+      window.addEventListener('mousemove', mousemove);
+      window.addEventListener('mouseup', mouseup);
+      
+      let prevX = e.x;
+      const leftPanel = leftPane.getBoundingClientRect();
+      
+      
+      function mousemove(e) {
+        let newX = prevX - e.x;
+        leftPane.style.width = leftPanel.width - newX + "px";
+      }
+      
+      function mouseup() {
+        window.removeEventListener('mousemove', mousemove);
+        window.removeEventListener('mouseup', mouseup);
+        
+      }
+      
+      
+    }
+    
+    
+    gutter.addEventListener('mousedown', resizer);
+
 
     var elem = document.querySelector('.main-carousel');
     var flkty = new Flickity(elem, {
